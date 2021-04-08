44365ad6f4451524475480964b1ef9df687cbe86
Joao Baumotte <jbaumotte@gmail.com>
2021-04-08 14:53:19 -0300

    Comic books API working

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -3,19 +3,34 @@ class GeneralController < ApplicationController
   end
 
   def show
-    @characters = find_character(params[:id])
+    @character = find_character(params[:id])["data"]['results'][0]
   end
 
   def search
-    puts params
-    @characters = find_characters(params[:character])
-    @characters = @characters["data"]['results']
+    @characters = find_characters(params[:character])["data"]['results']
 
     unless @characters
       flash[:alert] = 'Character not found'
       return render action: :index
     end
   end
+
+  #### COMIC RELATED #######
+  def search_comic
+    @comics = find_comics(params[:comic])["data"]['results']
+
+    unless @comics
+      flash[:alert] = 'Comic Book not found'
+      return render action: :index
+    end
+
+  end
+
+  def comic
+    @comic = find_comic(params[:id])["data"]['results'][0]
+  end
+
+
 
 
 
@@ -56,6 +71,20 @@ class GeneralController < ApplicationController
   def find_character(id)
     request_api(
       "https://gateway.marvel.com:443/v1/public/characters/#{id}"
+    )
+  end
+
+  def find_comics(comic)
+    comic_hash = {titleStartsWith: comic}
+    request_api(
+      "https://gateway.marvel.com:443/v1/public/comics",
+      comic_hash
+    )
+  end
+
+  def find_comic(id)
+    request_api(
+      "https://gateway.marvel.com:443/v1/public/comics/#{id}"
     )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'general#index'
   get '/search' => 'general#search'
   get "/character/:id" => 'general#show', as: "character"
+
+  get '/search_comic' => 'general#search_comic', as: "search_comic"
+  get "/comic/:id" => 'general#comic', as: "comic"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

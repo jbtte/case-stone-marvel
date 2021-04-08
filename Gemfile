545b47145f8ae1868b4728660858bec6acec91ff
Joao Baumotte <jbaumotte@gmail.com>
2021-04-08 09:09:27 -0300

    API call working

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.5'
 # Use postgresql as the database for Active Record
@@ -38,7 +40,7 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
-  gem 'dotenv-rails'
+  gem 'dotenv-rails', '~> 2.1', '>= 2.1.1'
 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -3,30 +3,42 @@ class GeneralController < ApplicationController
   end
 
   def search
-    caracter = find_caracter(params[:caracter])
-    unless caracter
-      flash[:alert] = 'Caracter not found'
+    puts params
+    @character = find_character(params[:character])
+
+    unless @character
+      flash[:alert] = 'Character not found'
       return render action: :index
     end
   end
 
   private
 
-  def request_api(url)
+  def request_api(url, dict)
+    # API Login info
+    query = {
+      ts: Rails.application.credentials.marvel[:TS],
+      apikey: Rails.application.credentials.marvel[:API_KEY],
+      hash: Rails.application.credentials.marvel[:HASH],
+    }
+
+    # Specific information of the query is added
+    query.merge!(dict)
+
     response = Excon.get(
       url,
-      Params: {
-        "apikey": "your api key",
-        "ts": "a timestamp",
-        "hash": "your hash"
-      }
+      query: query
     )
+
+    puts JSON.parse(response.body)
     return nil if response.status != 200
     JSON.parse(response.body)
   end
-  def find_caracter(caracter)
+  def find_character(character)
+    char_hash = {name: character}
     request_api(
-      "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
+      "https://gateway.marvel.com:443/v1/public/characters",
+      {name: character}
     )
   end
 end

4e6087a83c99b2883a62a724046fc9efc000a051
Joao Baumotte <jbaumotte@gmail.com>
2021-04-11 11:16:14 -0300

    Favorite model - unit test

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,4 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-
+  validates :user_id, :number, :name, presence: true
 end
--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class FavoriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save favorite without number" do
+    favorite = Favorite.new
+    favorite.user_id = 1
+    favorite.name = "Hello World"
+    favorite.character = true
+    assert_not favorite.save, "Saved favorite without number"
+  end
+
+  test "should not save favorite without name" do
+    favorite = Favorite.new
+    favorite.user_id = 1
+    favorite.number = 1324
+    favorite.character = true
+    assert_not favorite.save, "Saved favorite without name of character"
+  end
+
+  test "should not save favorite without user_id" do
+    favorite = Favorite.new
+    favorite.name = "Spider-man"
+    favorite.number = 1324
+    favorite.character = true
+    assert_not favorite.save, "Saved favorite without user_id"
+  end
+
+  test "should not save favorite without character definition" do
+    favorite = Favorite.new
+    favorite.name = "Spider-man"
+    favorite.number = 1324
+    favorite.user_id = 1
+    assert_not favorite.save, "Saved favorite without character definition
+    "
+  end
 end
